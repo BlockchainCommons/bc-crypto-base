e162ad840c9b8bbbcbf00c00cc35a4eb79892742
Mark Friedenbach <mark@friedenbach.org>
2020-05-25 03:53:01 -0700

    Add memory barrier to our implementation of memzero (taken from BoringSSL).
    The existing code uses the volatile keyword to try to get the compiler to not optimize away the memory wiping loop.  This should work on most compilers, though I'm not convinced it will work on all.  The other alternative, used in the Linux kernel, Google's BoringSSL, and Bitcoin Core is to use assembly intrinsics to issue a memory barrier.  This forces the C++ compiler (which lacks architectural knowledge) to back off.  Both approaches are complementary; this additional code gives us strictly greater assurance that the memory wipe is actually performed.
    
    Note that this particular fix was originally copied from BoringSSL, which was released by Google under the ISC license, which is compatible with bc-crypto-base's BSD license.

--- src/memzero.c
+++ src/memzero.c
@@ -64,5 +64,21 @@ void memzero(void *const pnt, const size_t len) {
   while (i < len) {
     pnt_[i++] = 0U;
   }
+
+  /* Memory barrier that scares the compiler away from optimizing out
+   * the above loop.
+   *
+   * Quoting Adam Langley <agl@google.com> in commit
+   * ad1907fe73334d6c696c8539646c21b11178f20f of BoringSSL (ISC License):
+   *
+   *    As best as we can tell, this is sufficient to break any optimisations
+   *    that might try to eliminate "superfluous" memsets.  This method is used
+   *    in memzero_explicit() the Linux kernel, too.  Its advantage is that it
+   *    is pretty efficient because the compiler can still implement the
+   *    memset() efficiently, just not remove it entirely.  See "Dead Store
+   *    Elimination (Still) Considered Harmful" by Yang et al. (USENIX Security
+   *    2017) for more background.
+   */
+  __asm__ __volatile__("" : : "r"(pnt_) : "memory");
 #endif
 }
